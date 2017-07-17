12f072e6793f363559f324335babf254a287c70f
johanna stephanie garate cardenas <johanna stephanie garate cardenas>
2017-07-17 14:15:28 -0500

    cambios44

---
+++ main.js
@@ -0,0 +1,15 @@
+var peso = parseFloat("Ingrese su peso")
+function planetas(){
+	
+	var peso_mercurio = peso * 3.7;
+	var peso_venus=peso * 8.87;
+	var peso_tierra=peso * 9.807;
+	var peso_marte=peso * 3.711;
+
+	 alert("tu peso en mercurio es" + peso_mercurio);
+	 alert("tu peso en venus es" + peso_venus);
+	 alert("tu peso en tierra es" + peso_tierra);
+	 alert("tu peso en martes es" + peso_marte);
+}
+
+planetas()
